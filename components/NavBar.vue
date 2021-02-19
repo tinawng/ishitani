718f3a5da18ff3a71979920f50404f3f27100424
Tina Wang <tina@tina.cafe>
2021-02-19 16:09:36 +0100

    🚧 Working on home page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,61 +1,28 @@
 <template>
-  <section>
-    <div class="z-50 nav-container flex items-center justify-end">
-      <transition name="slide-fade">
-        <div v-if="show_nav" class="w-full flex justify-end mr-4">
-          <h4 @click="scrollTo('hero')">About</h4>
-          <h4 @click="scrollTo('projects')">Projects</h4>
-          <h4 @click="scrollTo('footer')">Links</h4>
-        </div>
-      </transition>
-      <div
-        class="burger-container flex flex-col justify-center items-end"
-        @click="show_nav = !show_nav"
-      >
-        <span class="w-5" :class="[show_nav ? 'mb-0' : 'mb-1.5']"></span>
-        <span
-          class="w-3 parent-hover:w-5"
-          :class="{ 'opacity-0': show_nav }"
-        ></span>
-      </div>
+  <div class="nav-container">
+    <h2>ishitani furniture</h2>
+    <div class="nav-menu">
+      <h3>design</h3>
+      <h3>shop</h3>
+      <h3>diary</h3>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    show_nav: false,
-  }),
-  methods: {
-    scrollTo(section_name) {
-      var el = this.$parent.$parent.$el.getElementsByClassName(section_name)[0];
-      el.scrollIntoView({behavior: "smooth"});
-    }
-  }
-};
+export default {};
 </script>
 
 <style lang="postcss" scoped>
 .nav-container {
-  @apply h-32;
-}
-.nav-container h4 {
-  @apply mx-4;
-  @apply cursor-pointer;
+  @apply m-8;
+  @apply flex justify-between items-center;
 }
 
-.burger-container {
-  @apply h-full;
-  @apply cursor-pointer;
+.nav-menu {
+    @apply flex items-center
 }
-.burger-container span {
-  height: 2px;
-  @apply bg-steel-800;
-  transition: all 0.4s;
-  transition-property: margin, width, opacity;
-}
-.burger-container:hover .parent-hover\:w-5 {
-  @apply w-5;
+.nav-menu h3 {
+    @apply mx-8;
 }
 </style>
